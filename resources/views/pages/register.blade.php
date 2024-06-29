5238c1c506f3f3d58ab466aa770b15274d44ba7d
newbiedos <inidimas13@gmail.com>
2024-06-29 14:26:47 +0700

    update-final

--- resources/views/pages/register.blade.php
+++ resources/views/pages/register.blade.php
@@ -12,7 +12,7 @@
                 <a href="" class="-intro-x flex items-center pt-5">
                     <img alt="Tinker Tailwind HTML Admin Template" class="w-6" src="{{ asset('dist/images/logo.svg') }}">
                     <span class="text-white text-lg ml-3">
-                        Inkindo
+                        Bawaslu Surabaya
                     </span>
                 </a>
                 <div class="my-auto">
